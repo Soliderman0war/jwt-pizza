55a1ca066ed1657a4abea94ce2d6bb4a9418905b
drake <drakeamueller@gmail.com>
2025-09-04 17:43:55 -0500

    "require" in tailwind.config.js wasn't working properly so had to update npx and import fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+import { fontFamily as _fontFamily } from 'tailwindcss/defaultTheme';
 
-module.exports = {
-  content: ['index.html', './src/**/*.{html,js,tsx}', './node_modules/preline/preline.js'],
-  theme: {
-    extend: {
-      animation: {
-        wobble: 'wobble .5s ease-in-out',
-      },
-      keyframes: {
-        wobble: {
-          '0%, 100%': { transform: 'rotate(0deg)' },
-          '25%': { transform: 'rotate(-5deg)' },
-          '75%': { transform: 'rotate(5deg)' },
-        },
-      },
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+export const content = ['index.html', './src/**/*.{html,js,tsx}', './node_modules/preline/preline.js'];
+export const theme = {
+  extend: {
+    animation: {
+      wobble: 'wobble .5s ease-in-out',
+    },
+    keyframes: {
+      wobble: {
+        '0%, 100%': { transform: 'rotate(0deg)' },
+        '25%': { transform: 'rotate(-5deg)' },
+        '75%': { transform: 'rotate(5deg)' },
       },
     },
+    fontFamily: {
+      sans: ['Inter var', ..._fontFamily.sans],
+    },
   },
-  plugins: [require('preline/plugin')],
 };
+export const plugins = [require('preline/plugin')];
